c98825e756e9fc2c36957817ba4c83129ab60e13
Garrett van Wageningen <ghvanw283@gmail.com>
2024-09-30 13:37:38 -0500

    add http request and more souping

--- src/main.py
+++ src/main.py
@@ -1,2 +1,48 @@
+import aiohttp
+import asyncio
+from itertools import chain
+from bs4 import BeautifulSoup
 
 print("hello world")
+
+def replace_anchor_text(paragraph: BeautifulSoup) -> None:
+    for anchor in filter(lambda it: it is not None, paragraph.find_all("a")):
+        anchor.replace_with(anchor.get_text())
+
+
+
+def get_marvel_character_text(html: str) -> list[str]:
+    soup = BeautifulSoup(html, "html.parser")
+
+    results = list(
+        chain.from_iterable(
+            map(
+                lambda it: it.find_all("p"), 
+                soup.find_all(class_="text"))))
+
+    for item in results:
+        replace_anchor_text(item)
+
+    return results
+
+
+async def main():
+    async with aiohttp.ClientSession() as session:
+        # async with session.get("https://www.marvel.com/characters/venom-eddie-brock/in-comics") as response:
+        async with session.get("https://www.marvel.com/characters/wolverine-logan/in-comics") as response:
+
+            print("Status:", response.status)
+            print("Content-type:", response.headers['content-type'])
+
+            html = await response.text()
+
+            results = get_marvel_character_text(html)
+
+            print("#results", len(results))
+
+            for it in results:
+                print(it)
+
+
+
+asyncio.run(main())
